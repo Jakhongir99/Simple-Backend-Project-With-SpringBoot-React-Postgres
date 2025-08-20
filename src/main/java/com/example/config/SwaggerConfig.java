52c14f008b9cdcdb106aba23da9b36cbb8ef44f8
Jakhongir <jahongir@xb.uz>
2025-08-20 16:29:15 +0500

    fix:swagger config

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -1,19 +1,14 @@
 package com.example.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Value("${swagger.api.title:Java Simple CRUD API}")
@@ -44,24 +39,19 @@ public class SwaggerConfig {
     private String licenseUrl;
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title(apiTitle)
-                .description(apiDescription)
-                .version(apiVersion)
-                .termsOfServiceUrl(termsOfServiceUrl)
-                .contact(new Contact(contactName, contactUrl, contactEmail))
-                .license(licenseName)
-                .licenseUrl(licenseUrl)
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(apiTitle)
+                        .description(apiDescription)
+                        .version(apiVersion)
+                        .termsOfService(termsOfServiceUrl)
+                        .contact(new Contact()
+                                .name(contactName)
+                                .url(contactUrl)
+                                .email(contactEmail))
+                        .license(new License()
+                                .name(licenseName)
+                                .url(licenseUrl)));
     }
 }
