aeb1ebd2fc93afd911366b525afad685d1a7ce75
Jakhongir <jahongir@xb.uz>
2025-08-26 12:22:11 +0500

    fix:unused fields

--- clear_liquibase_lock.sql
+++
@@ -1,3 +0,0 @@
--- Clear Liquibase lock to fix startup issue
-DELETE FROM public.databasechangeloglock WHERE locked = true;
-UPDATE public.databasechangeloglock SET locked = false, lockgranted = null, lockedby = null;
